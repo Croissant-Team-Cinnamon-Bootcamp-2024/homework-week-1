e24e0ca11f6a46b327ddda27a158b189936fc002
Hung Pham <phamhung20022015@gmail.com>
2024-06-30 17:53:43 +0700

    fix(model): changing input of detector to numpy array and removing redundant code

--- src/ocr_app/model/detector.py
+++ src/ocr_app/model/detector.py
@@ -2,26 +2,24 @@ from typing import Tuple
 
 import cv2
 import numpy as np
-from PIL import Image
 
 
 class TextDetector:
-    def resize_image(
-        self, image: Image.Image, max_size: Tuple[int, int] = (2000, 2000)
-    ) -> Tuple[Image.Image, float]:
+    def _resize_image(
+        self, img: np.ndarray, max_size: Tuple[int, int] = (2000, 2000)
+    ) -> Tuple[np.ndarray, float]:
         """
         Resize the input image while maintaining aspect ratio
         and ensuring it fits within `max_size`.
 
         Args:
-            image (Image.Image): Input PIL image to be resized.
+            img (np.ndarray): Input image as a numpy array to be resized.
             max_size (Tuple[int, int]): Maximum allowed size (height, width)
             for the resized image. Defaults to (2000, 2000).
 
         Returns:
-            Tuple[Image.Image, float]: Resized PIL image and the scaling factor applied.
+            Tuple[np.ndarray, float]: Resized image as a numpy array and the scaling factor applied.
         """
-        img = np.array(image)
         height, width = img.shape[:2]
 
         if height > max_size[0] or width > max_size[1]:
@@ -45,27 +43,26 @@ class TextDetector:
         else:
             scaling_factor = 1.0
 
-        return Image.fromarray(img), float(scaling_factor)
+        return img, float(scaling_factor)
 
-    def preprocess_image(self, image: Image.Image) -> Tuple[np.ndarray, float]:
+    def preprocess_image(self, img: np.ndarray) -> Tuple[np.ndarray, float]:
         """
         Preprocess the input image by resizing and applying adaptive thresholding.
 
         Args:
-            image (Image.Image): Input PIL image to be preprocessed.
+            img (np.ndarray): Input image as a numpy array to be preprocessed.
 
         Returns:
             Tuple[np.ndarray, float]: Preprocessed image as a numpy array and
             the scaling factor applied.
         """
-        resized_image, scaling_factor = self.resize_image(image)
-        img = np.array(resized_image)
+        resized_image, scaling_factor = self._resize_image(img)
 
         # Check if the image is already in grayscale
-        if len(img.shape) == 2:
-            gray = img
+        if len(resized_image.shape) == 2:
+            gray = resized_image
         else:
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            gray = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
 
         adaptive_threshold = cv2.adaptiveThreshold(
             gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2
