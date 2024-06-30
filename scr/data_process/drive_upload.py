964a1077c5aa6f4b86e90c1220580af8450a1870
Kieu Son Tung <kieusontung8@gmail.com>
2024-06-30 11:06:22 +0700

    xong test

--- scr/data_process/drive_upload.py
+++ scr/data_process/drive_upload.py
@@ -2,10 +2,13 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import os
 
-google_auth = GoogleAuth()
-drive_app = GoogleDrive(google_auth)
-
 folder_id = '1dGRj7HaiKj2YZNcYZA3MB55ZhrcoXaYM'
+current_dir = os.path.dirname(os.path.abspath(__file__))
+client_secrets_file = os.path.join(current_dir, 'client_secrets.json')
+
+google_auth = GoogleAuth()
+google_auth.settings['client_config_file'] = client_secrets_file
+drive_app = GoogleDrive(google_auth)
 
 def get_files_from_results():
     results_dir = 'results'
@@ -15,16 +18,32 @@ def get_files_from_results():
 upload_files = get_files_from_results()
 
 # Function to delete all files in a folder
-def delete_files_in_folder(folder_id):
+def delete_files_in_folder(folder_id = folder_id):
     file_list = drive_app.ListFile({'q': f"'{folder_id}' in parents and trashed=false"}).GetList()
     for file in file_list:
         print(f"Deleting file: {file['title']}")
         file.Delete()
 
 
-def upload_file(file_path_list = upload_files):
+# def upload_file(file_path_list = upload_files):
+#     for file in file_path_list:
+#         upfile =  drive_app.CreateFile({'title':file[8:],'parents':[{'id':folder_id}]})
+#         upfile.SetContentFile(file)
+#         upfile.Upload()
+
+def upload_file(file_path_list=upload_files):
     for file in file_path_list:
-        upfile =  drive_app.CreateFile({'title':file[5:],'parents':[{'id':folder_id}]})
+        file_name = os.path.basename(file)
+        upfile = drive_app.CreateFile({'title': file_name, 'parents': [{'id': folder_id}]})
         upfile.SetContentFile(file)
         upfile.Upload()
+        print(f"Uploaded file: {file_name}")
 
+def file_exists_in_folder(folder_id, file_name):
+    query = f"'{folder_id}' in parents and trashed=false and title='{file_name}'"
+    file_list = drive_app.ListFile({'q': query}).GetList()
+    return len(file_list) > 0
+
+if __name__ == '__main__':
+    upload_file()
+    # delete_files_in_folder()
--- scr/data_process/test_drive_upload.py
+++ scr/data_process/test_drive_upload.py
@@ -1,6 +1,7 @@
 import drive_upload
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
+import os
 
 google_auth = GoogleAuth()
 drive_app = GoogleDrive(google_auth)
@@ -16,17 +17,16 @@ def test_delete_files_in_folder():
         print("The folder is not empty.")
 
 def test_upload_files():
-    file_names = drive_upload.get_files_from_results()
-    drive_upload.upload_file(file_names)   
-    def file_exists_in_folder(folder_id, file_name):
-        query = f"'{folder_id}' in parents and trashed=false and title='{file_name}'"
-        file_list = drive_app.ListFile({'q': query}).GetList()
-        return len(file_list) > 0
-    for file_name in file_names:
-        if file_exists_in_folder(drive_upload.folder_id, file_name):
-            print(f"The file '{file_name}' already exists in the folder.")
+    file_paths = drive_upload.get_files_from_results()
+    drive_upload.upload_file(file_paths)
+    
+    for file_path in file_paths:
+        file_name = os.path.basename(file_path)
+        if drive_upload.file_exists_in_folder(drive_upload.folder_id, file_name):
+            print(f"The file '{file_name}' exists in the folder.")
         else:
             print(f"The file '{file_name}' does not exist in the folder.")
-            return
-test_delete_files_in_folder()
-# test_upload_files()
+
+if __name__ == "__main__":
+    test_delete_files_in_folder()
+    test_upload_files()
