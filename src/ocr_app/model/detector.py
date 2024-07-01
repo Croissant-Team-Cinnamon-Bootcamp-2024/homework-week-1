046d3753424a5e6041f8884f5d7d66ac4da97b53
huy.ng <nguyenduchuy1711@gmail.com>
2024-07-01 23:07:07 +0700

    reformat code

--- src/ocr_app/model/detector.py
+++ src/ocr_app/model/detector.py
@@ -5,7 +5,9 @@ import numpy as np
 
 
 class TextDetector:
-    def _resize_image(self, img: np.ndarray, max_size: Tuple[int, int] = (2000, 2000)) -> Tuple[np.ndarray, float]:
+    def _resize_image(
+        self, img: np.ndarray, max_size: Tuple[int, int] = (2000, 2000)
+    ) -> Tuple[np.ndarray, float]:
         """
         Resize the input image while maintaining aspect ratio
         and ensuring it fits within `max_size`.
@@ -62,6 +64,8 @@ class TextDetector:
         else:
             gray = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
 
-        adaptive_threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+        adaptive_threshold = cv2.adaptiveThreshold(
+            gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2
+        )
 
         return adaptive_threshold, float(scaling_factor)
